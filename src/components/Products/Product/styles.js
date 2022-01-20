975d13e72a95f6505832e605cef110e1d95876fa
abdulkareem1094 <abdulak4t7@gmail.com>
2022-01-20 23:40:52 +0100

    fixed some styles prob

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -1,58 +1,56 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
-    // root:{
-    //     width: '180px',
-    //     boxShadow: '2px 2px 20px rgba(0,0,0,0.3)',
-    //     height: '300px'
-    // },
-    // img:{
-    //     height: '200px',
-    // },
-    // media:{
-    //     width: '100%',
-    //     height: '100%',
-    //     objectFit: 'cover',
-    //     boxSizing: 'border-box',
-    // },
-    // addToCartButtonVisible:{
-    //     width: '100%',
-    //     border: 'none',
-    //     color: '#ffffff',
-    //     backgroundColor: 'orange',
-    //     height: '15%',
-    //     marginTop: '85%'
-    // },
-    // addToCartButtonHidden:{
-    //     visibility: 'hidden',
-    // },
-    // cardContainer:{
-    //     display: 'flex',
-    //     fontFamily: 'calibri',
-    //     width: '100%'
-    // },
-    // cardContent:{
-    //     padding: '10px',
-    //     display: 'flex',
-    //     alignItems: 'center',
-    //     backgroundColor: ''
-    // },
-    // cardHeader:{
-    //     fontSize: '0.9rem',
-    //     color: '#222222',
-    //     fontWeight: 700,
-    //     letterSpacing: '0.5px',
-    //     paddingRight: '5px'
-    // },
-    // cardPrice:{
-    //     fontSize: '0.9rem',
-    //     letterSpacing: '0.5px',
-    //     paddingLeft: '5px',
-    // },
-    // cardDesc:{
-    //     fontSize: '0.7rem'
-    // },
-    // grow:{
-    //     flex: 1
-    // }
+    root:{
+        width: '150px',
+        boxShadow: '2px 2px 5px rgba(0,0,0,0.3)',
+        height: '200px'
+    },
+    img:{
+        height: '150px',
+    },
+    media:{
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        boxSizing: 'border-box',
+    },
+    addToCartButtonVisible:{
+        width: '100%',
+        border: 'none',
+        color: '#ffffff',
+        backgroundColor: 'orange',
+        height: '15%',
+        marginTop: '85%'
+    },
+    addToCartButtonHidden:{
+        visibility: 'hidden',
+    },
+    cardContainer:{
+        display: 'flex',
+        fontFamily: 'calibri',
+        width: '100%'
+    },
+    cardContent:{
+        padding: '5px 10px',
+        backgroundColor: ''
+    },
+    cardHeader:{
+        fontSize: '0.6rem',
+        fontWeight: 600,
+        letterSpacing: '0.5px',
+        paddingRight: '5px'
+    },
+    cardPrice:{
+        fontSize: '0.6rem',
+        letterSpacing: '0.5px',
+        paddingLeft: '5px',
+        fontWeight: 600,
+    },
+    cardDesc:{
+        fontSize: '0.4rem'
+    },
+    grow:{
+        flex: 1
+    }
 }));
