2c32c3de65dd2cf0d2b36f7ddf719ead4ad8b2c0
abdulkareem1094 <abdulak4t7@gmail.com>
2022-01-24 01:29:12 +0100

    Working on NavBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
+import useStyles from "./styles";
 
 const Navbar = () => {
+
+    const classes = useStyles();
+
     return (
         <>
-            <AppBar position='fixed' className={classes.appBar}>
+            <AppBar position='fixed' className={classes.appBar} color='inherit'>
                 <Toolbar>
-                        
+                        <Typography variant='h6' className={classes.title} color='inherit'>
+                            <img src="" alt="KS" height='25px' className={classes.image} />
+                            Kareem Store
+                        </Typography>
+                        <div className={classes.grow}/>
+                        <div className={classes.button}>
+                            <IconButton area-lebel='Show Cart Item' color='inherit'>
+                                <Badge badgeContent='2' color='secundry'>
+                                    <ShoppingCart  />
+                                </Badge>
+                            </IconButton>
+                        </div>
                 </Toolbar>    
             </AppBar>   
         </>
